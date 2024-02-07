11ab0370ab95eb1aebe8a62ae270567d1a480758
RecceRaven <anthonyramdeen782@gmail.com>
2024-02-07 14:06:15 -0500

    added initial components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css'
+import About from "./Components/Pages/About/About"
+import Home from "./Components/Pages/Home/Home"
+import Checkout from "./Components/Pages/Checkout/Checkout"
+import Landing from "./Components/Pages/Landing/Landing"
+import Profile from "./Components/Pages/Profile/Profile"
+import Store from "./Components/Pages/Store/Store"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <nav>
+        {/* Navigation Links */}
+        <Link to="/">Landing</Link> |{" "}
+        <Link to="/home">Home</Link> |{" "}
+        <Link to="/about">About</Link> |{" "}
+        <Link to="/checkout">Checkout</Link> |{" "}
+        <Link to="/profile">Profile</Link> |{" "}
+        <Link to="/store">Store</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/store" element={<Store />} />
+        {/* Redirect all other paths to Landing or a 404 Not Found component */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
