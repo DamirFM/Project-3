9569efd4623ad86a636d131a7277c99aebdf226e
Wesley Noble <wrn007@hotmail.com>
2024-02-12 09:16:07 -0500

    stash commit

--- server/seeds/productData.js
+++ server/seeds/productData.js
@@ -1,157 +1,156 @@
 // seeds/productData.js
 
 const products = [
-    //LAPTOPS
-    {
-      name: "Alienware X16 R1 Gaming Laptop",
-      description: "A powerful laptop with excellent performance for professional use.",
-      image: "https://m.media-amazon.com/images/I/71YFV8SQthL._AC_SL1500_.jpg",
-      quantity: 50,
-      price: 4000.00,
-      category: "65c26d9303c196854fd60ec0"
-    },
-    {
-      name: "ASUS ROG Strix G15",
-      description: "The latest AMD Ryzen 7 6800H CPU paired with a NVIDIA GeForce RTX 3060 Laptop GPU and MUX Switch form the backbone of this powerful gaming laptop",
-      image: "https://m.media-amazon.com/images/I/71OyrTkxpGL._AC_SL1500_.jpg",
-      quantity: 75,
-      price: 999.99,
-      category: "65c26d9303c196854fd60ec0"
-    },
-    {
-      name: "Dell G16 7630 Gaming Laptop",
-      description: "16-inch QHD+ (2560x1600) 165Hz Display, Intel Core i9-13900HX Processor, 16GB DDR5 RAM, 1TB SSD, NVIDIA GeForce RTX 4070 GDDR6, Windows 11 Home - Metallic Nightshade",
-      image: "https://m.media-amazon.com/images/I/712Qo4PFS8L._AC_SL1500_.jpg",
-      quantity: 30,
-      price: 2099.99,
-      category: "65c26d9303c196854fd60ec0"
-    },
-    {
-      name: "Alienware X17 R2 FHD 360Hz Display",
-      description: "12th Gen Intel Core i9-12900H 14-Core up to 5.00 GHz CPU, 32GB DDR5 RAM, 8TB (2X 4TB) NVMe SSD, GeForce RTX 3070Ti 8GB GDDR6, Windows 11 Home",
-      image: "https://m.media-amazon.com/images/I/81K8AZ1jsEL._AC_SL1500_.jpg",
-      quantity: 40,
-      price: 7299.99,
-      category: "65c26d9303c196854fd60ec0"
-    },
-    {
-      name: "Dell Alienware M16 R1 165Hz WQXGA (2560x1600) IPS Laptop",
-      description: "13th Generation Intel Core i7-13700HX 16-Core Processor (Up to 5.0 GHz with Intel Turbo Boost Technology, 30 MB Intel Smart cache, 16 Cores: 8 Performance + 8 Efficient, 24 Threads)",
-      image: "https://m.media-amazon.com/images/I/7197+rLcZeL._AC_SL1168_.jpg",
+  //LAPTOPS
+  {
+    name: "Alienware X16 R1 Gaming Laptop",
+    description: "A powerful laptop with excellent performance for professional use.",
+    image: "https://m.media-amazon.com/images/I/71YFV8SQthL._AC_SL1500_.jpg",
+    quantity: 50,
+    price: 4000.00,
+    category: "65c26d9303c196854fd60ec0"
+  },
+  {
+    name: "ASUS ROG Strix G15",
+    description: "The latest AMD Ryzen 7 6800H CPU paired with a NVIDIA GeForce RTX 3060 Laptop GPU and MUX Switch form the backbone of this powerful gaming laptop",
+    image: "https://m.media-amazon.com/images/I/71OyrTkxpGL._AC_SL1500_.jpg",
+    quantity: 75,
+    price: 999.99,
+    category: "65c26d9303c196854fd60ec0"
+  },
+  {
+    name: "Dell G16 7630 Gaming Laptop",
+    description: "16-inch QHD+ (2560x1600) 165Hz Display, Intel Core i9-13900HX Processor, 16GB DDR5 RAM, 1TB SSD, NVIDIA GeForce RTX 4070 GDDR6, Windows 11 Home - Metallic Nightshade",
+    image: "https://m.media-amazon.com/images/I/712Qo4PFS8L._AC_SL1500_.jpg",
+    quantity: 30,
+    price: 2099.99,
+    category: "65c26d9303c196854fd60ec0"
+  },
+  {
+    name: "Alienware X17 R2 FHD 360Hz Display",
+    description: "12th Gen Intel Core i9-12900H 14-Core up to 5.00 GHz CPU, 32GB DDR5 RAM, 8TB (2X 4TB) NVMe SSD, GeForce RTX 3070Ti 8GB GDDR6, Windows 11 Home",
+    image: "https://m.media-amazon.com/images/I/81K8AZ1jsEL._AC_SL1500_.jpg",
+    quantity: 40,
+    price: 7299.99,
+    category: "65c26d9303c196854fd60ec0"
+  },
+  {
+    name: "Dell Alienware M16 R1 165Hz WQXGA (2560x1600) IPS Laptop",
+    description: "13th Generation Intel Core i7-13700HX 16-Core Processor (Up to 5.0 GHz with Intel Turbo Boost Technology, 30 MB Intel Smart cache, 16 Cores: 8 Performance + 8 Efficient, 24 Threads)",
+    image: "https://m.media-amazon.com/images/I/7197+rLcZeL._AC_SL1168_.jpg",
+    quantity: 60,
+    price: 850.00,
+    category: "65c26d9303c196854fd60ec0"
+  },
+  {
+      name: "HP Victus FHD Gaming Laptop",
+      description: "144Hz, Intel Core i5-13420H (8 Core) Processor, 8GB RAM, 512GB SSD, Windows 11, GeForce RTX 3050 6GB GDDR6 Graphics - Performance Blue",
+      image: "https://m.media-amazon.com/images/I/71gfWwWhd4L._AC_SL1500_.jpg",
       quantity: 60,
-      price: 850.00,
+      price: 2859.98,
       category: "65c26d9303c196854fd60ec0"
     },
     {
-        name: "HP Victus FHD Gaming Laptop",
-        description: "144Hz, Intel Core i5-13420H (8 Core) Processor, 8GB RAM, 512GB SSD, Windows 11, GeForce RTX 3050 6GB GDDR6 Graphics - Performance Blue",
-        image: "https://m.media-amazon.com/images/I/71gfWwWhd4L._AC_SL1500_.jpg",
-        quantity: 60,
-        price: 2859.98,
-        category: "65c26d9303c196854fd60ec0"
-      },
-      {
-        name: "Acer Nitro 17 FHD Laptop",
-        description: "AMD Ryzen 5 7535HS, NVIDIA GeForce RTX 4050, 16GB RAM, 512GB SSD, Windows 11 Home",
-        image: "https://m.media-amazon.com/images/I/51Jm3X90VrL._AC_SL1280_.jpg",
-        quantity: 60,
-        price: 1456.93,
-        category: "65c26d9303c196854fd60ec0"
-      },
-      {
-        name: "ASUS TUF Gaming F15 (2023) Gaming Laptop",
-        description: "144Hz Display, GeForce RTX 4050, Intel® Core™ i9-13900H, 16GB DDR4, 512GB PCIe SSD, Wi-Fi 6, Windows 11",
-        image: "https://m.media-amazon.com/images/I/71NxKpg1NIL._AC_SL1500_.jpg",
-        quantity: 60,
-        price: 1249.99,
-        category: "65c26d9303c196854fd60ec0"
-      },
-      {
-        name: "ASUS TUF A17 17.3",
-        description: "AMD Ryzen 5 4600H, NVIDIA GeForce GTX 1650, 32GB RAM, 1TB PCIe SSD, RGB Backlit Keyboard, Windows 11, Bonfire Black, 32GB Hotface USB Card",
-        image: "https://m.media-amazon.com/images/I/81Mn3w0ZqXL._AC_SL1500_.jpg",
-        quantity: 60,
-        price: 1850.00,
-        category: "65c26d9303c196854fd60ec0"
-      },
-      {
-        name: "HP Victus FHD Gaming Laptop",
-        description: "Intel Core i5-13420H Processor, 32GB RAM, 1TB PCIe SSD, NVIDIA GeForce RTX 3050, Backlit Keyboard, HD Webcam, Windows 11, Blue, 32GB Hotface USB Card",
-        image: "https://m.media-amazon.com/images/I/81sqA4El0VL._AC_SL1500_.jpg",
-        quantity: 60,
-        price: 1250.00,
-        category: "65c26d9303c196854fd60ec0"
-      },
+      name: "Acer Nitro 17 FHD Laptop",
+      description: "AMD Ryzen 5 7535HS, NVIDIA GeForce RTX 4050, 16GB RAM, 512GB SSD, Windows 11 Home",
+      image: "https://m.media-amazon.com/images/I/51Jm3X90VrL._AC_SL1280_.jpg",
+      quantity: 60,
+      price: 1456.93,
+      category: "65c26d9303c196854fd60ec0"
+    },
+    {
+      name: "ASUS TUF Gaming F15 (2023) Gaming Laptop",
+      description: "144Hz Display, GeForce RTX 4050, Intel® Core™ i9-13900H, 16GB DDR4, 512GB PCIe SSD, Wi-Fi 6, Windows 11",
+      image: "https://m.media-amazon.com/images/I/71NxKpg1NIL._AC_SL1500_.jpg",
+      quantity: 60,
+      price: 1249.99,
+      category: "65c26d9303c196854fd60ec0"
+    },
+    {
+      name: "ASUS TUF A17 17.3",
+      description: "AMD Ryzen 5 4600H, NVIDIA GeForce GTX 1650, 32GB RAM, 1TB PCIe SSD, RGB Backlit Keyboard, Windows 11, Bonfire Black, 32GB Hotface USB Card",
+      image: "https://m.media-amazon.com/images/I/81Mn3w0ZqXL._AC_SL1500_.jpg",
+      quantity: 60,
+      price: 1850.00,
+      category: "65c26d9303c196854fd60ec0"
+    },
+    {
+      name: "HP Victus FHD Gaming Laptop",
+      description: "Intel Core i5-13420H Processor, 32GB RAM, 1TB PCIe SSD, NVIDIA GeForce RTX 3050, Backlit Keyboard, HD Webcam, Windows 11, Blue, 32GB Hotface USB Card",
+      image: "https://m.media-amazon.com/images/I/81sqA4El0VL._AC_SL1500_.jpg",
+      quantity: 60,
+      price: 1250.00,
+      category: "65c26d9303c196854fd60ec0"
+    },
 
 //DESKTOPS
 
 {
-    name: "Skytech Chronos",
-    description: "Intel Core i5 12400F 2.5 GHz, RTX 3060, 500GB NVME SSD, 16G DDR4 3200, 650W Gold PSU, AC Wi-Fi, Windows 10 Home 64-bit",
-    image: "https://m.media-amazon.com/images/I/81GFCsdD6UL._AC_SL1498_.jpg",
-    quantity: 60,
-    price: 1300.00,
-    category: "65c26d9303c196854fd60ec1"
-  },
-  {
-    name: "Skytech Nebula",
-    description: "Intel Core i3 12100F 3.3 GHz, NVIDIA GTX 1650, 500GB NVME SSD, 16GB DDR4 RAM 3200, 600W Gold PSU, 11AC Wi-Fi, Windows 11 Home 64-bit",
-    image: "https://m.media-amazon.com/images/I/61INtCQzv2L._AC_SL1000_.jpg",
-    quantity: 60,
-    price: 875.00,
-    category: "65c26d9303c196854fd60ec1"
-  },
-  {
-    name: "ABYS MAX Prebuild Graphic Designer PC",
-    description: "Graphic Designer PC (Intel i5 Up to 3.60 GHz, 16GB, 240GB SSD, 1TB HDD, 1GB Nvidia Quadro Video Card, ARGB, WiFi, Windows 10, English/Spanish/French) Video Editing, Gaming Computer",
-    image: "https://m.media-amazon.com/images/I/71Ihy747EvL._AC_SL1500_.jpg",
-    quantity: 60,
-    price: 350,
-    category: "65c26d9303c196854fd60ec1"
-  },
-  {
-    name: "HP RGB Gaming Desktop Computer",
-    description: "Intel Quad Core I7 3.4G up to 3.9GHz, GeForce GT 1030 2G, 16GB RAM, 512G SSD, New 24 1080 FHD LED, RGB KB & MS, 600M WiFi & Bluetooth 5.0, Win10 Pro",
-    image: "https://m.media-amazon.com/images/I/71ZGErOg4gL._AC_SL1500_.jpg",
-    quantity: 60,
-    price: 400,
-    category: "65c26d9303c196854fd60ec1"
-  },
-  {
-    name: "Skytech Chronos",
-    description: "Intel Core i5 12400F 2.5 GHz, NVIDIA RTX 4060, 1TB NVME SSD, 16GB DDR4 RAM 3200, 650W Gold PSU, 11AC Wi-Fi, Windows 11 Home 64-bit",
-    image: "https://m.media-amazon.com/images/I/91RsXVV1m1L._AC_SL1500_.jpg",
-    quantity: 60,
-    price: 1300,
-    category: "65c26d9303c196854fd60ec1"
-  },
-  {
-    name: "MSI Codex R 12NUC5-066CA",
-    description: "Intel Core i5-12400F, GeForce RTX 4060, 16GB DDR5 Memory (Dual-CHL), 1TB SSD, WiFi 6, USB Type-C, VR-Ready, Windows 11 Home",
-    image: "https://m.media-amazon.com/images/I/81RWuA9UR3L._AC_SL1500_.jpg",
-    quantity: 60,
-    price: 1450,
-    category: "65c26d9303c196854fd60ec1"
-  },
-  {
-    name: "AQVIN Gaming Computer",
-    description: "Intel i7 6th Gen, GeForce RTX 3060 12GB GDDR6, 32GB RAM DDR4 1TB SSD Storage, RGB Keyboard and Mouse, WiFi Ready Windows 10 Pro",
-    image: "https://m.media-amazon.com/images/I/715X30JtU2L._AC_SL1500_.jpg",
-    quantity: 60,
-    price: 1100,
-    category: "65c26d9303c196854fd60ec1"
-  },
-  {
-    name: "Skytech Azure",
-    description: "AMD Ryzen 7 7700X 4.5 GHz, NVIDIA",
-    image: "https://m.media-amazon.com/images/I/91CnBmI3G8L._AC_SL1500_.jpg",
-    quantity: 60,
-    price: 2200,
-    category: "65c26d9303c196854fd60ec1"
-  }
+  name: "Skytech Chronos",
+  description: "Intel Core i5 12400F 2.5 GHz, RTX 3060, 500GB NVME SSD, 16G DDR4 3200, 650W Gold PSU, AC Wi-Fi, Windows 10 Home 64-bit",
+  image: "https://m.media-amazon.com/images/I/81GFCsdD6UL._AC_SL1498_.jpg",
+  quantity: 60,
+  price: 1300.00,
+  category: "65c26d9303c196854fd60ec1"
+},
+{
+  name: "Skytech Nebula",
+  description: "Intel Core i3 12100F 3.3 GHz, NVIDIA GTX 1650, 500GB NVME SSD, 16GB DDR4 RAM 3200, 600W Gold PSU, 11AC Wi-Fi, Windows 11 Home 64-bit",
+  image: "https://m.media-amazon.com/images/I/61INtCQzv2L._AC_SL1000_.jpg",
+  quantity: 60,
+  price: 875.00,
+  category: "65c26d9303c196854fd60ec1"
+},
+{
+  name: "ABYS MAX Prebuild Graphic Designer PC",
+  description: "Graphic Designer PC (Intel i5 Up to 3.60 GHz, 16GB, 240GB SSD, 1TB HDD, 1GB Nvidia Quadro Video Card, ARGB, WiFi, Windows 10, English/Spanish/French) Video Editing, Gaming Computer",
+  image: "https://m.media-amazon.com/images/I/71Ihy747EvL._AC_SL1500_.jpg",
+  quantity: 60,
+  price: 350,
+  category: "65c26d9303c196854fd60ec1"
+},
+{
+  name: "HP RGB Gaming Desktop Computer",
+  description: "Intel Quad Core I7 3.4G up to 3.9GHz, GeForce GT 1030 2G, 16GB RAM, 512G SSD, New 24 1080 FHD LED, RGB KB & MS, 600M WiFi & Bluetooth 5.0, Win10 Pro",
+  image: "https://m.media-amazon.com/images/I/71ZGErOg4gL._AC_SL1500_.jpg",
+  quantity: 60,
+  price: 400,
+  category: "65c26d9303c196854fd60ec1"
+},
+{
+  name: "Skytech Chronos",
+  description: "Intel Core i5 12400F 2.5 GHz, NVIDIA RTX 4060, 1TB NVME SSD, 16GB DDR4 RAM 3200, 650W Gold PSU, 11AC Wi-Fi, Windows 11 Home 64-bit",
+  image: "https://m.media-amazon.com/images/I/91RsXVV1m1L._AC_SL1500_.jpg",
+  quantity: 60,
+  price: 1300,
+  category: "65c26d9303c196854fd60ec1"
+},
+{
+  name: "MSI Codex R 12NUC5-066CA",
+  description: "Intel Core i5-12400F, GeForce RTX 4060, 16GB DDR5 Memory (Dual-CHL), 1TB SSD, WiFi 6, USB Type-C, VR-Ready, Windows 11 Home",
+  image: "https://m.media-amazon.com/images/I/81RWuA9UR3L._AC_SL1500_.jpg",
+  quantity: 60,
+  price: 1450,
+  category: "65c26d9303c196854fd60ec1"
+},
+{
+  name: "AQVIN Gaming Computer",
+  description: "Intel i7 6th Gen, GeForce RTX 3060 12GB GDDR6, 32GB RAM DDR4 1TB SSD Storage, RGB Keyboard and Mouse, WiFi Ready Windows 10 Pro",
+  image: "https://m.media-amazon.com/images/I/715X30JtU2L._AC_SL1500_.jpg",
+  quantity: 60,
+  price: 1100,
+  category: "65c26d9303c196854fd60ec1"
+},
+{
+  name: "Skytech Azure",
+  description: "AMD Ryzen 7 7700X 4.5 GHz, NVIDIA",
+  image: "https://m.media-amazon.com/images/I/91CnBmI3G8L._AC_SL1500_.jpg",
+  quantity: 60,
+  price: 2200,
+  category: "65c26d9303c196854fd60ec1"
+}
 
 
-  ];
-  
-  module.exports = products;
-  
+];
+
+module.exports = products;
