2cefb2eed0c66768e7798a7ae01188d58471df3f
DamirFM <yakupovdamir93@gmail.com>
2024-05-22 22:51:38 -0400

    add

--- server/server.js
+++ server/server.js
@@ -182,7 +182,7 @@ app.post('/api/orders', async (req, res) => {
     app.use(express.static(path.join(__dirname, '../client/dist')));
 
     app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile(path.join(__dirname, '../client/index.html'));
     });
   }
 
